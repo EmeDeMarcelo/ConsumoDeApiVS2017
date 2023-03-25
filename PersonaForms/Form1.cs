13b2ef7be883faf734c0cad9bf92b5809cba4a5b
Marcelo <marcelojara23@gmail.com>
2023-03-25 17:44:07 -0300

    Agregar archivos de proyecto.

---
+++ PersonaForms/Form1.cs
@@ -0,0 +1,88 @@
+﻿using PersonaForms.Models.Request;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+
+namespace PersonaForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = "http://localhost:63134/api/Persona";
+            PersonaRequest oPersona = new PersonaRequest();
+            oPersona.Nombre = txtNombre.Text;
+            oPersona.Edad = int.Parse( txtEdad.Text);
+            
+
+            //primer parametro url
+            //segundo parametro el objeto
+            //tercer parametro el metodo: POST, GET, PUT, DELETE
+            string resultado = Send<PersonaRequest>(url, oPersona, "POST");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public string Send<T>(string url, T objectRequest, string method = "POST")
+        {
+            string result = "";
+
+            try
+            {
+                
+
+                JavaScriptSerializer js = new JavaScriptSerializer();
+
+                //serializamos el objeto
+                string json = Newtonsoft.Json.JsonConvert.SerializeObject(objectRequest);
+
+                //peticion
+                WebRequest request = WebRequest.Create(url);
+                //headers
+                request.Method = method;
+                request.PreAuthenticate = true;
+                request.ContentType = "application/json;charset=utf-8'";
+                request.Timeout = 10000; //esto es opcional
+
+                using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                {
+                    streamWriter.Write(json);
+                    streamWriter.Flush();
+                }
+
+                var httpResponse = (HttpWebResponse)request.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    result = streamReader.ReadToEnd();
+                }
+               
+            }
+            catch (Exception e)
+            {
+
+                result = e.Message;
+                
+
+            }
+
+            return result;
+        }
+    }
+}
+
---
+++ PersonaForms/Models/Request/PersonaRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonaForms.Models.Request
+{
+    public class PersonaRequest
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+    }
+}
